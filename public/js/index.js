8da20b4cdbc5e4316bf8c42141ac7406f6306be4
Don <hello@donbos.co>
2018-11-25 15:41:27 +0800

    Add mustache js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -15,25 +15,30 @@ socket.on('disconnect', function(){
 
 // Fires when there is a new message that is being emitted from the server
 socket.on('newMessage', function(message){
-	// console.log("New incoming message", message);
 	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var li = $('<li></li>');
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
+	// $('#messages').append(li);
 
-	$('#messages').append(li);
+	var template = $('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
+
+	$('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var li = $('<li></li>');
-	var a = $('<a target="_blank">My current location</a>');
+	var template = $('#location-message-template').html();
+	
+	var html = Mustache.render(template, {
+		from: message.from,
+		url: message.url,
+		createdAt: formattedTime
+	});
 
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url);
-
-	li.append(a);
-
-	$('#messages').append(li);
+	$('#messages').append(html);
 })
 
 $('#message-form').on('submit', function(e) {
